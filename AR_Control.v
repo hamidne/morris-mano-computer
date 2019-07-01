dae48cb5f5f48b7f8bed1fe39f353df718d0b944
HamidNE <hamidne@mail.ru>
2019-07-01 22:23:17 +0430

    clean up Ar Controller code

--- AR_Control.v
+++ AR_Control.v
@@ -1,20 +1,19 @@
-بmodule AR_Control(Load, CLR, INR, T, D, R, I);	
+module AR_Control(LD, CLR, INR, T, D, R, I);	
 	input R, I;
 	input [7:0] T, D;
-	output Load, CLR, INR;
+	output LD, CLR, INR;
 	
 	wire D7n, Rn;
-	wire a1, a2, a3;  
+	wire a1, a2, a3;
 	
-	assign D7n= ~D[7];	
-	assign Rn= ~R;
+	assign D7n = ~D[7];
+	assign Rn = ~R;
 	
-	
-	assign a1= D7n & I & T[3];
-	assign a2= T[2] & Rn;
-	assign a3= Rn & T[0];
-	assign CLR= T[0] & R;
-	assign INR= D[5] & T[4];
-	assign Load= a1 | a2 | a3;
+	assign a1 = D7n & I & T[3];
+	assign a2 = T[2] & Rn;
+	assign a3 = Rn & T[0];
+	assign CLR = T[0] & R;
+	assign INR = D[5] & T[4];
+	assign LD = a1 | a2 | a3;
 	
 endmodule

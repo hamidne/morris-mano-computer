8170acddb185ad346a6a5c06ea8bbaeee115314e
HamidNE <hamidne@mail.ru>
2019-07-02 16:23:47 +0430

    edit for IEN_FlipFlop

--- Flip-Flop/IEN_FlipFlop.v
+++ Flip-Flop/IEN_FlipFlop.v
@@ -1,3 +1,18 @@
-module IEN_FlipFlop();
+module IEN_FlipFlop(IEN, D, T, B, I, R, CLK);
+
+    output IEN;
+    input [0:7]D, [0:7]T, [0:7]B I, R;
+
+    wire p, j, k, k1, k2;
+
+    assign p = D[7] & I & T[3];
+    assign j = p & B[7];
+
+    assign k1 = p & B[6];
+    assign k2 = R & T[2];
+
+    assign k = k1 | k2;
+
+    
 
 endmodule
